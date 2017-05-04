d5e956d5030566ee333a02ce334c97c2b20aa8a8
Oshri Ifrah <oshri.ifrah@intel.com>
2017-05-04 11:10:58 +0300

    Change App.ts

--- app.ts
+++ app.ts
@@ -1,7 +1,11 @@
-import { Person, Pilot } from "./examples";
+import { Person, Phone, Pilot, flightTicket } from "./examples";
 
-let p1 = new Person("Oshri", 33);
+let p1: Person = new Person("Oshri", 33);
 let p2 = new Pilot("Dan", 30, 3000, 2);
 
+let tst = {
+    a: 1,
+    b: 2
+}
+
 console.log(p1.personInformation());
-console.log(p2.printPilotDetails());
